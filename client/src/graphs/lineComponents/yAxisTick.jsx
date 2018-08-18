3fee6d99fc59414763c72faf182ca9ba28583548
Jae <jaebae1111@gmail.com>
2018-08-17 19:23:14 -0700

    added tracing circle

--- client/src/graphs/lineComponents/yAxisTick.jsx
+++ client/src/graphs/lineComponents/yAxisTick.jsx
@@ -7,7 +7,7 @@ class YAxisTick extends Component {
 
   componentDidMount () {
     document.getElementById("y-" + this.props.ind).style.position = "absolute";
-    document.getElementById("y-" + this.props.ind).style.top = `${this.props.factor * 355 + 5}px`;
+    document.getElementById("y-" + this.props.ind).style.top = `${this.props.factor * 370}px`;
     document.getElementById("y-" + this.props.ind).style.right = "-100%";
     document.getElementById("y-" + this.props.ind).style.zIndex = "1";
     document.getElementById("y-" + this.props.ind).style.paddingTop = "25px";
@@ -18,7 +18,7 @@ class YAxisTick extends Component {
   render () {
     return (
       <span id={"y-" + this.props.ind}>
-        <div id={"y" + this.props.ind}>{this.props.yValue}</div>
+        <div id={"y" + this.props.ind}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{this.props.yValue}</div>
       </span>
     );
   }
